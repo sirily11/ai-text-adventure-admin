cd219c44eef600dfbab78af510599259d9582116
sirily11 <sirily1997@gmail.com>
2023-03-30 10:12:33 +0800

    Initial Commit

---
+++ ai-text-adventure-admin/ai_text_adventure_adminApp.swift
@@ -0,0 +1,17 @@
+//
+//  ai_text_adventure_adminApp.swift
+//  ai-text-adventure-admin
+//
+//  Created by Qiwei Li on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ai_text_adventure_adminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
